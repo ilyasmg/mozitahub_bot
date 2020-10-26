8e11dfba44a225916677f5dad58855c9b229fdfb
dag7dev <44711271+dag7dev@users.noreply.github.com>
2020-10-26 20:03:13 +0100

    added raiserexceptioner in settings.py in case env variables are not defined

--- settings.py
+++ settings.py
@@ -2,5 +2,11 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-TOKEN = os.getenv("TOKEN")
-NEWS_CHANNEL = os.getenv("NEWS_CHANNEL")
+def is_env_defined(variable, throws=None):
+    if variable in os.environ:
+        os.getenv(variable)
+    else:
+        raise Exception(variable + " is not present. Check your .env file and try again!")
+
+TOKEN = is_env_defined("TOKEN")
+NEWS_CHANNEL = is_env_defined("NEWS_CHANNEL")
